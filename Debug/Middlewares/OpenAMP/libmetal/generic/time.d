205f75e9c729916fbc97ed12f805ce922521ebb0
DMGDy <dylangarza1909@gmail.com>
2024-09-25 18:55:56 +0200

    initial commit

---
+++ Debug/Middlewares/OpenAMP/libmetal/generic/time.d
@@ -0,0 +1,18 @@
+Middlewares/OpenAMP/libmetal/generic/time.o: \
+ /home/osboxes/STM32CubeMP1/Middlewares/Third_Party/OpenAMP/libmetal/lib/system/generic/time.c \
+ ../../../../../../../../Middlewares/Third_Party/OpenAMP/libmetal/lib/include/metal/time.h \
+ ../../../../../../../../Middlewares/Third_Party/OpenAMP/libmetal/lib/include/metal/sys.h \
+ ../../../../../../../../Middlewares/Third_Party/OpenAMP/libmetal/lib/include/metal/log.h \
+ ../../../../../../../../Middlewares/Third_Party/OpenAMP/libmetal/lib/include/metal/system/generic/log.h \
+ ../../../../../../../../Middlewares/Third_Party/OpenAMP/libmetal/lib/include/metal/list.h \
+ ../../../../../../../../Middlewares/Third_Party/OpenAMP/libmetal/lib/include/metal/system/generic/sys.h \
+ ../../../../../../../../Middlewares/Third_Party/OpenAMP/libmetal/lib/include/metal/errno.h \
+ ../../../../../../../../Middlewares/Third_Party/OpenAMP/libmetal/lib/include/metal/system/generic/./template/sys.h
+../../../../../../../../Middlewares/Third_Party/OpenAMP/libmetal/lib/include/metal/time.h:
+../../../../../../../../Middlewares/Third_Party/OpenAMP/libmetal/lib/include/metal/sys.h:
+../../../../../../../../Middlewares/Third_Party/OpenAMP/libmetal/lib/include/metal/log.h:
+../../../../../../../../Middlewares/Third_Party/OpenAMP/libmetal/lib/include/metal/system/generic/log.h:
+../../../../../../../../Middlewares/Third_Party/OpenAMP/libmetal/lib/include/metal/list.h:
+../../../../../../../../Middlewares/Third_Party/OpenAMP/libmetal/lib/include/metal/system/generic/sys.h:
+../../../../../../../../Middlewares/Third_Party/OpenAMP/libmetal/lib/include/metal/errno.h:
+../../../../../../../../Middlewares/Third_Party/OpenAMP/libmetal/lib/include/metal/system/generic/./template/sys.h:
---
+++ README.md
@@ -0,0 +1,3 @@
+# STM32CubeIDE Project for using IPC with OpenAMP
+Load project with STM32CubeIDE
+
