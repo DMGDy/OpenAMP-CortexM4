eec357629bf6032a955a73b40c470c6d1512dc69
DMGDy <dylangarza1909@gmail.com>
2024-09-27 09:25:12 +0200

    modified to only use UART0 which is /dev/ttyRPMSG0 and added cases for messages

---
+++ OpenAMP_TTY_echo_CM4/Debug/Application/User/syscalls.d
@@ -0,0 +1,2 @@
+Application/User/syscalls.o: \
+ /home/osboxes/STM32CubeMP1/Projects/STM32MP157C-DK2/Applications/OpenAMP/OpenAMP_TTY_echo/STM32CubeIDE/CM4/Application/User/syscalls.c
---
+++ OpenAMP_TTY_echo_CM4/Debug/Drivers/BSP/stm32mp15xx_disco.d
@@ -0,0 +1,55 @@
+Drivers/BSP/stm32mp15xx_disco.o: \
+ /home/osboxes/STM32CubeMP1/Drivers/BSP/STM32MP15xx_DISCO/stm32mp15xx_disco.c \
+ /home/osboxes/STM32CubeMP1/Drivers/BSP/STM32MP15xx_DISCO/stm32mp15xx_disco.h \
+ ../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal.h \
+ ../../../Inc/stm32mp1xx_hal_conf.h \
+ ../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_rcc.h \
+ ../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_def.h \
+ ../../../../../../../../Drivers/CMSIS/Device/ST/STM32MP1xx/Include/stm32mp1xx.h \
+ ../../../../../../../../Drivers/CMSIS/Device/ST/STM32MP1xx/Include/stm32mp157cxx_cm4.h \
+ ../../../../../../../../Drivers/CMSIS/Include/core_cm4.h \
+ ../../../../../../../../Drivers/CMSIS/Include/cmsis_version.h \
+ ../../../../../../../../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../../../../../../../../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../../../../../../../../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../../../../../../../../Drivers/CMSIS/Device/ST/STM32MP1xx/Include/system_stm32mp1xx.h \
+ ../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_rcc_ex.h \
+ ../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_exti.h \
+ ../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_hsem.h \
+ ../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_gpio.h \
+ ../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_gpio_ex.h \
+ ../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_dma.h \
+ ../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_dma_ex.h \
+ ../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_cortex.h \
+ ../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_ipcc.h \
+ ../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_pwr.h \
+ ../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_pwr_ex.h \
+ ../../../Inc/stm32mp15xx_disco_conf.h ../../../Inc/lock_resource.h \
+ /home/osboxes/STM32CubeMP1/Drivers/BSP/STM32MP15xx_DISCO/stm32mp15xx_disco_errno.h
+/home/osboxes/STM32CubeMP1/Drivers/BSP/STM32MP15xx_DISCO/stm32mp15xx_disco.h:
+../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal.h:
+../../../Inc/stm32mp1xx_hal_conf.h:
+../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_rcc.h:
+../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_def.h:
+../../../../../../../../Drivers/CMSIS/Device/ST/STM32MP1xx/Include/stm32mp1xx.h:
+../../../../../../../../Drivers/CMSIS/Device/ST/STM32MP1xx/Include/stm32mp157cxx_cm4.h:
+../../../../../../../../Drivers/CMSIS/Include/core_cm4.h:
+../../../../../../../../Drivers/CMSIS/Include/cmsis_version.h:
+../../../../../../../../Drivers/CMSIS/Include/cmsis_compiler.h:
+../../../../../../../../Drivers/CMSIS/Include/cmsis_gcc.h:
+../../../../../../../../Drivers/CMSIS/Include/mpu_armv7.h:
+../../../../../../../../Drivers/CMSIS/Device/ST/STM32MP1xx/Include/system_stm32mp1xx.h:
+../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_rcc_ex.h:
+../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_exti.h:
+../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_hsem.h:
+../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_gpio.h:
+../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_gpio_ex.h:
+../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_dma.h:
+../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_dma_ex.h:
+../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_cortex.h:
+../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_ipcc.h:
+../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_pwr.h:
+../../../../../../../../Drivers/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal_pwr_ex.h:
+../../../Inc/stm32mp15xx_disco_conf.h:
+../../../Inc/lock_resource.h:
+/home/osboxes/STM32CubeMP1/Drivers/BSP/STM32MP15xx_DISCO/stm32mp15xx_disco_errno.h:
